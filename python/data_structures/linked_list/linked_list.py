34cec191cdc9654868aa3d8b4f53e86b321632ea
bjgman12 <bjgman38@gmail.com>
2020-12-28 08:45:53 -0800

    one function working

--- python/data_structures/linked_list/linked_list.py
+++ python/data_structures/linked_list/linked_list.py
@@ -34,6 +34,40 @@ class LinkedList:
                 curr = curr.retNext()
         return False
 
+    def append(self,value):
+        """
+        This function sets the current to the head value
+        and then set it equal to the next node if traverses
+        through the linked lsit untill there is no next value
+        and then sets a new next to current after escaping
+        the loop it makes a new next and stores our value inside
+        """
+        current = self.head
+        if current:
+            while current.next != None:
+                current = current.retNext()
+            current.next = Node(value)
+        else:
+            self.head = Node(value)
+
+    def insertAfter(self,chosen,value):
+        
+        current = self.head
+      
+        if current.next is chosen:
+            self.insert(value)
+            return 
+        
+        while current is not None:
+            if current.next is chosen:
+                new_node = Node(value)
+                new_node.next = current.next
+                current.next = node
+                return 
+            current = current.next
+            
+    
+
     def __str__(self):
         curr = self.head
         display = ''
@@ -46,10 +80,13 @@ class LinkedList:
         display += ' Null '
         return display
 
+
 if __name__ == "__main__":
     node = Node(12)
     linked = LinkedList(node)
     linked.insert(14)
     linked.insert(15)
     linked.insert(11)
+    linked.append(20)
+    linked.insertAfter(15,16)
     print(linked)
