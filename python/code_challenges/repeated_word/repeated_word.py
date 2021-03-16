12e0b62b71d5330a63f3434f7f8dd0c9a550254a
bjgman12 <bjgman38@gmail.com>
2021-03-15 20:44:00 -0700

    fixed punctuation removal

--- python/code_challenges/repeated_word/repeated_word.py
+++ python/code_challenges/repeated_word/repeated_word.py
@@ -4,7 +4,7 @@ import string
 
 def repeated_word_check(text_to_check):
     case_format = text_to_check.lower()
-    punctuation_clean = case_format.translate(string.punctuation)
+    punctuation_clean = case_format.translate(str.maketrans('','',string.punctuation))
     iterable_words = punctuation_clean.split()
     ht = Hashtable()
 
